5e2982c7e0807a506cd9bc62b9c36c26f765878d
hamlim <hamlim@rpi.edu>
2014-07-24 15:56:04 -0700

    Public Milestone 2 update 1
    Added Business section, updated old names and information in other files

---
+++ Public Milestone 2/store.php
@@ -0,0 +1,12 @@
+<?php
+	$data = file_get_contents("php://input");
+	$data = json_decode($data);
+	$time = time();
+	array_push($data, $time);
+	$sdata = serialize($data);
+	
+	$file = fopen("arr.txt", 'w');
+	fwrite($file, $sdata);
+	fclose($file);
+	
+?>
