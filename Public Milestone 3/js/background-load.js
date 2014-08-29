9a8920fb36dffb6831442686005f1b4c7683767d
hamlim <hamlim@rpi.edu>
2014-08-28 22:08:49 -0400

    some menial edits

--- Public Milestone 3/js/background-load.js
+++ Public Milestone 3/js/background-load.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
-  var h = $(window).height();
-  var w = $(window).width();
-  if( h < 5000 && w < 5000){
-    $('body').css('background-image', 'url(http://unsplash.it/' + w + '/' + h + '?random)');
-		$('body').css('background-size', '100%');
-		$('article').css('background-color', 'transparent');
-  } else {
-    $('body').css('background','#dddddd');
-  };
+	$('body').css('background-image', 'url(http://unsplash.it/5000/5000?random)');
+	$('body').css('background-size', '100%');
+	$('article').css('background-color', 'transparent');
 
 });
